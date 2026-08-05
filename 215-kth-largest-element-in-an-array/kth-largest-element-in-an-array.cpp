b79c21585675e933d17fe8cea0d9dcaaddbf0ad8
Shiv Rathore <71895109+SHIVANIUM-GIT@users.noreply.github.com>
2026-08-05 19:18:04 +0530

    Time: 8 ms (96.14%) | Memory: 69 MB (95.71%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -3,23 +3,22 @@ public:
     int findKthLargest(vector<int>& nums, int k) {
         int n = nums.size();
         k = n - k;
-      
+
         auto quickSelect = [&](auto&& quickSelect, int left, int right) -> int {
             if (left == right) {
                 return nums[left];
             }
-          
+
             int i = left - 1;
             int j = right + 1;
+
             int pivot = nums[(left + right) >> 1];
-          
+
             while (i < j) {
-                while (nums[++i] < pivot) {
-                }
-              
-                while (nums[--j] > pivot) {
-                }
-              
+                while (nums[++i] < pivot);
+
+                while (nums[--j] > pivot);
+
                 if (i < j) {
                     swap(nums[i], nums[j]);
                 }
@@ -28,9 +27,10 @@ public:
             if (j < k) {
                 return quickSelect(quickSelect, j + 1, right);
             }
+
             return quickSelect(quickSelect, left, j);
         };
-      
+
         return quickSelect(quickSelect, 0, n - 1);
     }
 };
