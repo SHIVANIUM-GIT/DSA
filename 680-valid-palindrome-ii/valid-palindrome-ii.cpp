37d74cb67d01b33c0f002ff427774a4e9d62520f
Shiv Rathore <71895109+SHIVANIUM-GIT@users.noreply.github.com>
2026-08-06 21:37:30 +0530

    Time: 1 ms (78.37%) | Memory: 26.6 MB (33.44%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    bool inpil(string s, int i, int j)
+    {
+        while (i <= j)
+        {
+            if (s[i] != s[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int n =s.size();
+        int i = 0;
+        int j = n-1;
+
+        while(i <= j)
+        {
+            if (s[i] != s[j]) return (inpil(s, i+1, j) || inpil(s,i ,j-1));
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
