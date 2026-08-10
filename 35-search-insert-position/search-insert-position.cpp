c7c5d32c9a04858768b2b96c220cbf18abad9ff6
Shiv Rathore <71895109+SHIVANIUM-GIT@users.noreply.github.com>
2026-08-10 11:34:07 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (44.11%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -11,13 +11,11 @@ public:
 
             if (nums[mid] == target)
                 return mid;
-
-            if (nums[mid] < target)
+            else if (nums[mid] < target)
                 left = mid + 1;
             else
                 right = mid - 1;
         }
-
         return left;
     }
 };
