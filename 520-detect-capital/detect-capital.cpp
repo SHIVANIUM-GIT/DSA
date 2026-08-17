5badb843f80cef2796f6f51116342d87bf9526d4
Shiv Rathore <71895109+SHIVANIUM-GIT@users.noreply.github.com>
2026-08-17 12:56:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (15.50%) - LeetSync

---
+++ 520-detect-capital/detect-capital.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int n = word.size();
+        int count = 0;
+
+        for(int i = 0; i < n; ++i)
+        {
+            if (isupper(word[i]))
+            {
+                ++count;
+            }
+        }
+        if(count == 1 && isupper(word[0]))
+        {
+            return true;
+        }
+        else if(count == 0)
+        {
+            return true;
+        }
+        else if( count == n)
+        {
+            return true;
+        }
+
+        return false;
+    }
+};
