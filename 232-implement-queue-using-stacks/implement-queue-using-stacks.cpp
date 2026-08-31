bff3f9fba5c77a502eb4d3c8ed68e9fabb01599f
Shiv Rathore <71895109+SHIVANIUM-GIT@users.noreply.github.com>
2026-08-31 19:11:03 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (47.43%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -0,0 +1,41 @@
+class MyQueue {
+private:
+    std::stack<int> in;
+    std::stack<int> out;
+
+public:
+    MyQueue() {
+    }
+
+    void push(int x) {
+        in.push(x);
+    }
+
+    int pop() {
+        if (out.empty()) {
+            while (!in.empty()) {
+                out.push(in.top());
+                in.pop();
+            }
+        }
+
+        int x = out.top();
+        out.pop();
+        return x;
+    }
+
+    int peek() {
+        if (out.empty()) {
+            while (!in.empty()) {
+                out.push(in.top());
+                in.pop();
+            }
+        }
+
+        return out.top();
+    }
+
+    bool empty() {
+        return in.empty() && out.empty();
+    }
+};
